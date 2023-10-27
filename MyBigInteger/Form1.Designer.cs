80d5c422707a75e75a9257b5c02deda57bced07b
Zawiaha <hromakase@mail.ru>
2023-10-27 22:51:09 +0300

    Добавьте файлы проекта.

---
+++ MyBigInteger/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace MyBigIntegerForm
+{
+    partial class mbiForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tNumFirst = new System.Windows.Forms.TextBox();
+            this.bDegree = new System.Windows.Forms.Button();
+            this.bRemOfDiv = new System.Windows.Forms.Button();
+            this.bCompLess = new System.Windows.Forms.Button();
+            this.bCompMore = new System.Windows.Forms.Button();
+            this.bComptLessOrEq = new System.Windows.Forms.Button();
+            this.bComptMoreOrEq = new System.Windows.Forms.Button();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.bDiff = new System.Windows.Forms.Button();
+            this.bDivision = new System.Windows.Forms.Button();
+            this.bMult = new System.Windows.Forms.Button();
+            this.bEquals = new System.Windows.Forms.Button();
+            this.bNotEqual = new System.Windows.Forms.Button();
+            this.bGetResult = new System.Windows.Forms.Button();
+            this.tNumSecond = new System.Windows.Forms.TextBox();
+            this.lOper = new System.Windows.Forms.Label();
+            this.gBResult = new System.Windows.Forms.GroupBox();
+            this.tResult = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBoxSecNum = new System.Windows.Forms.GroupBox();
+            this.lExSpeed = new System.Windows.Forms.Label();
+            this.gBInputForm = new System.Windows.Forms.GroupBox();
+            this.gBResult.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBoxSecNum.SuspendLayout();
+            this.gBInputForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tNumFirst
+            // 
+            this.tNumFirst.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tNumFirst.Location = new System.Drawing.Point(4, 22);
+            this.tNumFirst.Name = "tNumFirst";
+            this.tNumFirst.Size = new System.Drawing.Size(206, 35);
+            this.tNumFirst.TabIndex = 0;
+            // 
+            // bDegree
+            // 
+            this.bDegree.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bDegree.Location = new System.Drawing.Point(232, 179);
+            this.bDegree.Name = "bDegree";
+            this.bDegree.Size = new System.Drawing.Size(100, 30);
+            this.bDegree.TabIndex = 1;
+            this.bDegree.Text = "x¹";
+            this.bDegree.UseVisualStyleBackColor = true;
+            this.bDegree.Click += new System.EventHandler(this.degree_Click);
+            // 
+            // bRemOfDiv
+            // 
+            this.bRemOfDiv.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bRemOfDiv.Location = new System.Drawing.Point(338, 179);
+            this.bRemOfDiv.Name = "bRemOfDiv";
+            this.bRemOfDiv.Size = new System.Drawing.Size(100, 30);
+            this.bRemOfDiv.TabIndex = 2;
+            this.bRemOfDiv.Text = "mod";
+            this.bRemOfDiv.UseVisualStyleBackColor = true;
+            this.bRemOfDiv.Click += new System.EventHandler(this.remOfDiv_Click);
+            // 
+            // bCompLess
+            // 
+            this.bCompLess.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bCompLess.Location = new System.Drawing.Point(20, 107);
+            this.bCompLess.Name = "bCompLess";
+            this.bCompLess.Size = new System.Drawing.Size(100, 30);
+            this.bCompLess.TabIndex = 3;
+            this.bCompLess.Text = "<";
+            this.bCompLess.UseVisualStyleBackColor = true;
+            this.bCompLess.Click += new System.EventHandler(this.compLess_Click);
+            // 
+            // bCompMore
+            // 
+            this.bCompMore.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bCompMore.Location = new System.Drawing.Point(126, 107);
+            this.bCompMore.Name = "bCompMore";
+            this.bCompMore.Size = new System.Drawing.Size(100, 30);
+            this.bCompMore.TabIndex = 5;
+            this.bCompMore.Text = ">";
+            this.bCompMore.UseVisualStyleBackColor = true;
+            this.bCompMore.Click += new System.EventHandler(this.compMore_Click);
+            // 
+            // bComptLessOrEq
+            // 
+            this.bComptLessOrEq.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bComptLessOrEq.Location = new System.Drawing.Point(232, 107);
+            this.bComptLessOrEq.Name = "bComptLessOrEq";
+            this.bComptLessOrEq.Size = new System.Drawing.Size(100, 30);
+            this.bComptLessOrEq.TabIndex = 6;
+            this.bComptLessOrEq.Text = "≤";
+            this.bComptLessOrEq.UseVisualStyleBackColor = true;
+            this.bComptLessOrEq.Click += new System.EventHandler(this.comptLessOrEq_Click);
+            // 
+            // bComptMoreOrEq
+            // 
+            this.bComptMoreOrEq.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bComptMoreOrEq.Location = new System.Drawing.Point(338, 107);
+            this.bComptMoreOrEq.Name = "bComptMoreOrEq";
+            this.bComptMoreOrEq.Size = new System.Drawing.Size(100, 30);
+            this.bComptMoreOrEq.TabIndex = 7;
+            this.bComptMoreOrEq.Text = "≥";
+            this.bComptMoreOrEq.UseVisualStyleBackColor = true;
+            this.bComptMoreOrEq.Click += new System.EventHandler(this.comptMoreOrEq_Click);
+            // 
+            // bAdd
+            // 
+            this.bAdd.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bAdd.Location = new System.Drawing.Point(20, 143);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(100, 30);
+            this.bAdd.TabIndex = 10;
+            this.bAdd.Text = "+";
+            this.bAdd.UseVisualStyleBackColor = true;
+            this.bAdd.Click += new System.EventHandler(this.add_Click);
+            // 
+            // bDiff
+            // 
+            this.bDiff.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bDiff.Location = new System.Drawing.Point(126, 143);
+            this.bDiff.Name = "bDiff";
+            this.bDiff.Size = new System.Drawing.Size(100, 30);
+            this.bDiff.TabIndex = 11;
+            this.bDiff.Text = "-";
+            this.bDiff.UseVisualStyleBackColor = true;
+            this.bDiff.Click += new System.EventHandler(this.diff_Click);
+            // 
+            // bDivision
+            // 
+            this.bDivision.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bDivision.Location = new System.Drawing.Point(232, 143);
+            this.bDivision.Name = "bDivision";
+            this.bDivision.Size = new System.Drawing.Size(100, 30);
+            this.bDivision.TabIndex = 12;
+            this.bDivision.Text = "/";
+            this.bDivision.UseVisualStyleBackColor = true;
+            this.bDivision.Click += new System.EventHandler(this.division_Click);
+            // 
+            // bMult
+            // 
+            this.bMult.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bMult.Location = new System.Drawing.Point(338, 143);
+            this.bMult.Name = "bMult";
+            this.bMult.Size = new System.Drawing.Size(100, 30);
+            this.bMult.TabIndex = 13;
+            this.bMult.Text = "*";
+            this.bMult.UseVisualStyleBackColor = true;
+            this.bMult.Click += new System.EventHandler(this.mult_Click);
+            // 
+            // bEquals
+            // 
+            this.bEquals.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bEquals.Location = new System.Drawing.Point(20, 179);
+            this.bEquals.Name = "bEquals";
+            this.bEquals.Size = new System.Drawing.Size(100, 30);
+            this.bEquals.TabIndex = 14;
+            this.bEquals.Text = "=";
+            this.bEquals.UseVisualStyleBackColor = true;
+            this.bEquals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // bNotEqual
+            // 
+            this.bNotEqual.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bNotEqual.Location = new System.Drawing.Point(126, 179);
+            this.bNotEqual.Name = "bNotEqual";
+            this.bNotEqual.Size = new System.Drawing.Size(100, 30);
+            this.bNotEqual.TabIndex = 15;
+            this.bNotEqual.Text = "!=";
+            this.bNotEqual.UseVisualStyleBackColor = true;
+            this.bNotEqual.Click += new System.EventHandler(this.notEqual_Click);
+            // 
+            // bGetResult
+            // 
+            this.bGetResult.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bGetResult.Location = new System.Drawing.Point(20, 215);
+            this.bGetResult.Name = "bGetResult";
+            this.bGetResult.Size = new System.Drawing.Size(206, 30);
+            this.bGetResult.TabIndex = 16;
+            this.bGetResult.Text = "Результат";
+            this.bGetResult.UseVisualStyleBackColor = true;
+            this.bGetResult.Click += new System.EventHandler(this.getResult_Click);
+            // 
+            // tNumSecond
+            // 
+            this.tNumSecond.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tNumSecond.Location = new System.Drawing.Point(4, 26);
+            this.tNumSecond.Name = "tNumSecond";
+            this.tNumSecond.Size = new System.Drawing.Size(206, 35);
+            this.tNumSecond.TabIndex = 17;
+            // 
+            // lOper
+            // 
+            this.lOper.AutoSize = true;
+            this.lOper.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lOper.Location = new System.Drawing.Point(242, 55);
+            this.lOper.Name = "lOper";
+            this.lOper.Size = new System.Drawing.Size(0, 26);
+            this.lOper.TabIndex = 18;
+            // 
+            // gBResult
+            // 
+            this.gBResult.Controls.Add(this.tResult);
+            this.gBResult.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.gBResult.Location = new System.Drawing.Point(12, 12);
+            this.gBResult.Name = "gBResult";
+            this.gBResult.Size = new System.Drawing.Size(749, 70);
+            this.gBResult.TabIndex = 19;
+            this.gBResult.TabStop = false;
+            this.gBResult.Text = "Результат вычисления";
+            // 
+            // tResult
+            // 
+            this.tResult.Location = new System.Drawing.Point(6, 29);
+            this.tResult.Name = "tResult";
+            this.tResult.Size = new System.Drawing.Size(737, 35);
+            this.tResult.TabIndex = 24;
+            this.tResult.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tNumFirst);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(20, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(216, 67);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Первое число";
+            // 
+            // groupBoxSecNum
+            // 
+            this.groupBoxSecNum.Controls.Add(this.tNumSecond);
+            this.groupBoxSecNum.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBoxSecNum.Location = new System.Drawing.Point(285, 26);
+            this.groupBoxSecNum.Name = "groupBoxSecNum";
+            this.groupBoxSecNum.Size = new System.Drawing.Size(216, 67);
+            this.groupBoxSecNum.TabIndex = 21;
+            this.groupBoxSecNum.TabStop = false;
+            this.groupBoxSecNum.Text = "Второе число";
+            // 
+            // lExSpeed
+            // 
+            this.lExSpeed.AutoSize = true;
+            this.lExSpeed.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lExSpeed.Location = new System.Drawing.Point(12, 355);
+            this.lExSpeed.Name = "lExSpeed";
+            this.lExSpeed.Size = new System.Drawing.Size(24, 26);
+            this.lExSpeed.TabIndex = 22;
+            this.lExSpeed.Text = "1";
+            this.lExSpeed.Visible = false;
+            // 
+            // gBInputForm
+            // 
+            this.gBInputForm.Controls.Add(this.groupBoxSecNum);
+            this.gBInputForm.Controls.Add(this.groupBox2);
+            this.gBInputForm.Controls.Add(this.lOper);
+            this.gBInputForm.Controls.Add(this.bGetResult);
+            this.gBInputForm.Controls.Add(this.bNotEqual);
+            this.gBInputForm.Controls.Add(this.bEquals);
+            this.gBInputForm.Controls.Add(this.bMult);
+            this.gBInputForm.Controls.Add(this.bDivision);
+            this.gBInputForm.Controls.Add(this.bDiff);
+            this.gBInputForm.Controls.Add(this.bAdd);
+            this.gBInputForm.Controls.Add(this.bComptMoreOrEq);
+            this.gBInputForm.Controls.Add(this.bComptLessOrEq);
+            this.gBInputForm.Controls.Add(this.bCompMore);
+            this.gBInputForm.Controls.Add(this.bCompLess);
+            this.gBInputForm.Controls.Add(this.bRemOfDiv);
+            this.gBInputForm.Controls.Add(this.bDegree);
+            this.gBInputForm.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.gBInputForm.Location = new System.Drawing.Point(12, 88);
+            this.gBInputForm.Name = "gBInputForm";
+            this.gBInputForm.Size = new System.Drawing.Size(532, 264);
+            this.gBInputForm.TabIndex = 23;
+            this.gBInputForm.TabStop = false;
+            this.gBInputForm.Text = "Панель взаимодействия";
+            // 
+            // mbiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 402);
+            this.Controls.Add(this.gBInputForm);
+            this.Controls.Add(this.lExSpeed);
+            this.Controls.Add(this.gBResult);
+            this.Name = "mbiForm";
+            this.Text = "MyBigInteger";
+            this.gBResult.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBoxSecNum.ResumeLayout(false);
+            this.groupBoxSecNum.PerformLayout();
+            this.gBInputForm.ResumeLayout(false);
+            this.gBInputForm.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox tNumFirst;
+        private Button bDegree;
+        private Button bRemOfDiv;
+        private Button bCompLess;
+        private Button bCompMore;
+        private Button bComptLessOrEq;
+        private Button bComptMoreOrEq;
+        private Button bAdd;
+        private Button bDiff;
+        private Button bDivision;
+        private Button bMult;
+        private Button bEquals;
+        private Button bNotEqual;
+        private Button bGetResult;
+        private TextBox tNumSecond;
+        private Label lOper;
+        private GroupBox gBResult;
+        private GroupBox groupBox2;
+        private GroupBox groupBoxSecNum;
+        private Label lExSpeed;
+        private GroupBox gBInputForm;
+        private RichTextBox tResult;
+    }
+}
---
+++ MyBigInteger/Form1.cs
@@ -0,0 +1,219 @@
+﻿using MBIClassLib;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+
+namespace MyBigIntegerForm
+{
+    public partial class mbiForm : Form
+    {
+        private Stopwatch sWatch = new Stopwatch();
+        private const int standartSizeOfForm = 450;
+        private (int, int) sizeForm;
+        private (int, int) sizeTResult;
+        private (int, int) sizeGbResult;
+        private (int, int) sizeGbInputForm;
+        private (int, int) sizeLExSpeed;
+        public mbiForm()
+        {
+            InitializeComponent();
+            Size = new Size(Size.Width, standartSizeOfForm);
+            sizeForm = new(Size.Width, Size.Height);
+            sizeTResult = new(tResult.Width, tResult.Height);
+            sizeGbResult = new(gBResult.Width, gBResult.Height);
+            sizeGbInputForm = new(gBInputForm.Location.X, gBInputForm.Location.Y);
+            sizeLExSpeed = new(lExSpeed.Location.X, lExSpeed.Location.Y);
+        }
+
+        private void compLess_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "<";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void compMore_Click(object sender, EventArgs e)
+        {
+            lOper.Text = ">";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void comptLessOrEq_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "≤";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void comptMoreOrEq_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "≥";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "+";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void diff_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "-";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void division_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "/";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void mult_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "*";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "=";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void notEqual_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "≠";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void degree_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "^";
+            groupBoxSecNum.Text = "Показатель степени";
+        }
+
+        private void remOfDiv_Click(object sender, EventArgs e)
+        {
+            lOper.Text = "%";
+            groupBoxSecNum.Text = "Второе число";
+        }
+
+        private void getResult_Click(object sender, EventArgs e)
+        {
+            if (tNumFirst.Text != string.Empty && tNumSecond.Text != string.Empty)
+            {
+                if (lOper.Text != string.Empty)
+                {
+                    returnNormalSizeForm();
+                    lExSpeed.Text = "Время выполнения операции:";
+                    var n1 = new MyBigInteger(tNumFirst.Text);
+                    var n2 = new MyBigInteger(tNumSecond.Text);
+                    string result = "0";
+                    switch (lOper.Text)
+                    {
+                        case "+":
+                            getExcutionTime(() => {
+                                result = (n1 + n2).ToString();
+                            });
+                            break;
+                        case "-":
+                            getExcutionTime(() => {
+                                result = (n1 - n2).ToString();
+                            });
+                            break;
+                        case "<":
+                            getExcutionTime(() => {
+                                result = (n1 < n2).ToString();
+                            });
+                            break;
+                        case ">":
+                            getExcutionTime(() => {
+                                result = (n1 > n2).ToString();
+                            });
+                            break;
+                        case "≥":
+                            getExcutionTime(() => {
+                                result = (n1 >= n2).ToString();
+                            });
+                            break;
+                        case "≤":
+                            getExcutionTime(() => {
+                                result = (n1 <= n2).ToString();
+                            });
+                            break;
+                        case "^":
+                            getExcutionTime(() => {
+                                result = (n1 ^ n2).ToString();
+                            });
+                            break;
+                        case "*":
+                            getExcutionTime(() => {
+                                result = (n1 * n2).ToString();
+                            });
+                            break;
+                        case "/":
+                            getExcutionTime(() => {
+                                result = (n1 / n2).ToString();
+                            });
+                            break;
+                        case "%":
+                            getExcutionTime(() => {
+                                result = (n1 % n2).ToString();
+                            });
+                            break;
+                        case "=":
+                            getExcutionTime(() => {
+                                result = (n1 == n2).ToString();
+                            });
+                            break;
+                        case "≠":
+                            getExcutionTime(() => {
+                                result = (n1 != n2).ToString();
+                            });
+                            break;
+                        default:
+                            break;
+                    }
+                    writeResult(result);
+                    lExSpeed.Visible = true;
+                } else { MessageBox.Show("Выберите оперцию!"); }
+            } else { MessageBox.Show("Заполните все поля!"); }
+        }
+        
+        private void getExcutionTime(Action act)
+        {
+            sWatch.Start();
+            act();
+            sWatch.Stop();
+            lExSpeed.Text += $" {sWatch.Elapsed}";
+            sWatch.Reset();
+        }
+        private void writeResult(string result)
+        {
+            if (result.Length > 59)
+            {
+                int scaleLevel = result.Length / 59;
+                if (scaleLevel > 4)
+                    scaleLevel = 4;
+                autoScale(scaleLevel);
+                tResult.Text = result;
+            }
+            else { tResult.Text = result; }
+        }
+        private void autoScale(int scaleLevel)
+        {
+           Size = new Size(Size.Width, Size.Height + 35 * scaleLevel);
+           tResult.Size = new Size(tResult.Width, tResult.Height + 35 * scaleLevel);
+           gBResult.Size = new Size(gBResult.Width, gBResult.Height + 35 * scaleLevel);
+           gBInputForm.Location = new Point(gBInputForm.Location.X, gBInputForm.Location.Y + 35 * scaleLevel);
+           lExSpeed.Location = new Point(lExSpeed.Location.X, lExSpeed.Location.Y + 35 * scaleLevel);
+        }
+        private void returnNormalSizeForm()
+        {
+            Size = new Size(sizeForm.Item1, sizeForm.Item2);
+            tResult.Size = new Size(sizeTResult.Item1, sizeTResult.Item2);
+            gBResult.Size = new Size(sizeGbResult.Item1, sizeGbResult.Item2);
+            gBInputForm.Location = new Point(sizeGbInputForm.Item1, sizeGbInputForm.Item2);
+            lExSpeed.Location = new Point(sizeLExSpeed.Item1, sizeLExSpeed.Item2);
+        }
+        
+    }
+}
